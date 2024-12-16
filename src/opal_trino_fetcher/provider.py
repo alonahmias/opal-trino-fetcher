a4a61b0144cbe6d703a7ecf9acd4ce5cc94e1122
tamarinkeisari <tamarin500k@gmail.com>
2024-12-16 21:43:10 +0200

    finalize project

--- src/opal_trino_fetcher/provider.py
+++ src/opal_trino_fetcher/provider.py
@@ -5,13 +5,14 @@ from opal_common.fetcher.fetch_provider import BaseFetchProvider
 from opal_common.fetcher.events import FetcherConfig, FetchEvent
 
 from aiotrino.dbapi import connect, Connection, Cursor
+from aiotrino.auth import BasicAuthentication as BasicAuth
 
 
 class TrinoConnectionConfig(BaseModel):
-    host: str = Field(..., description="Trino host")
     port: int = Field(..., description="Trino port")
     user: str = Field(..., description="Trino user")
     password: str = Field(..., description="Trino password")
+    http_scheme: str = Field("http", description="Trino http scheme")
     catalog: str | None = Field(None, description="Trino catalog")
     source: str = Field("OPAL", description="Trino source")
     auth: Literal["BasicAuthentication"] = Field(
@@ -34,55 +35,66 @@ class TrinoFetcherConfig(FetcherConfig):
 
 class TrinoFetchEvent(FetchEvent):
     fetcher: str = "TrinoFetchProvider"
-    config: TrinoFetcherConfig = Field(..., description="Trino fetcher config")
+    config: TrinoFetcherConfig = None # type: ignore
 
 class TrinoFetchProvider(BaseFetchProvider):
     def __init__(self, event: TrinoFetchEvent):
-        self._event = event
-        self.__conn: Connection | None = None
-        self.__curr: Cursor | None = None
+        super().__init__(event)
+        self._conn: Connection | None = None
+        self._curr: Cursor | None = None
 
-    async def fetch_event(self, event: FetchEvent) -> TrinoFetchEvent:
-        return TrinoFetchEvent(**event.dict())
+    def parse_event(self, event: FetchEvent) -> TrinoFetchEvent:
+        return TrinoFetchEvent(**event.dict(exclude={"config"}), config=event.config)
     
     async def __aenter__(self):
         if self._event.config is None:
             raise RuntimeError("Config is not initialized")
-        self.__conn = connect(
-            host=self._event.config.connection_params.host,
+        if self._event.config.connection_params.http_scheme == "http":
+            self._conn = connect(
+            host=self._event.url,
             port=self._event.config.connection_params.port,
             user=self._event.config.connection_params.user,
-            password=self._event.config.connection_params.password,
             catalog=self._event.config.connection_params.catalog,
-            source=self._event.config.connection_params.source,
-            auth=self._event.config.connection_params.auth,
-        )
-        self.__curr = await self.__conn.cursor()
+            source=self._event.config.connection_params.source)
+        else:
+            self._conn = connect(
+                host=self._event.url,
+                port=self._event.config.connection_params.port,
+                user=self._event.config.connection_params.user,
+                catalog=self._event.config.connection_params.catalog,
+                source=self._event.config.connection_params.source,
+                auth=BasicAuth(username=self._event.config.connection_params.user,
+                        password=self._event.config.connection_params.password)
+            )
+        self._curr = await self._conn.cursor()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self.__curr:
-            await self.__curr.close()
-        if self.__conn:
-            await self.__conn.close()
+        if self._curr:
+            await self._curr.close()
+        if self._conn:
+            await self._conn.close()
 
     async def _fetch_(self) -> List[List[Any]] | List[Any]:
-        if self.__curr is None:
+        if self._curr is None:
             raise RuntimeError("Cursor is not initialized")
-        elif self._event.config.fetch_one: 
-            row = await self.__curr.fetchone()
+        
+        await self._curr.execute(self._event.config.query)
+
+        if self._event.config.fetch_one:
+            row = await self._curr.fetchone()
             if row is None:
                 return []
             return row
-        rows = await self.__curr.fetchall()
+        rows = await self._curr.fetchall()
         return rows
     
     async def _process_(self, records: List[List[Any]] | List[Any]) -> List[Dict[str, Any]] | Dict[str, Any]:
-        if self.__curr is None or self.__curr.description is None:
+        if self._curr is None or self._curr.description is None:
             raise RuntimeError("Cursor is not initialized")
         elif self._event.config.fetch_one:
             records = [records]
-        columns = [desc[0] for desc in self.__curr.description]
+        columns = [desc[0] for desc in self._curr.description]
         if self._event.config.fetch_key is None:
             return [dict(zip(columns, row)) for row in records]
         index_in_columns = columns.index(self._event.config.fetch_key)
